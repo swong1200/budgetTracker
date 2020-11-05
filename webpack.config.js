f56db50e208c64b06ac7a9adabe786d1e03b109c
Stuart Wong <swong31@gmail.com>
2020-11-05 15:42:55 -0800

    Ran npm build webpack; created manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,41 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
 const config = {
     entry: "./public/index.js",
     output: {
-        path: __dirname + "/dist",
+        path: __dirname + "/public/dist/",
         filename: "bundle.js"
     },
-    mode: "development"
+    mode: "development",
+    plugins: [new WebpackPwaManifest({
+        filename: "manifest.json",
+        inject: false,
+        fingerprints: false,
+        name: "Budget Tracker",
+        short_name: "Budget Track",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+            {
+                src: path.resolve(
+                    __dirname,
+                    "public/icons/icon-192x192.png"
+                    ),
+                    size: [72, 96, 128, 144, 152, 192, 384, 512]
+            },
+            {
+                src: path.resolve(
+                    __dirname,
+                    "public/icons/icon-512x512.png"
+                    ),
+                    size: [72, 96, 128, 144, 152, 192, 384, 512]
+            }
+            ]
+        })
+    ]
 };
 
 module.exports = config;
