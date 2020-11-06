ae31aeaf633e3e8016cc3f75c81776d030085412
Stuart Wong <swong31@gmail.com>
2020-11-06 14:41:26 -0800

    Created object store inside indexeddb

--- public/indexeddb.js
+++ public/indexeddb.js
@@ -1,5 +1,10 @@
 const request = indexedDB.open("budgetdb", 1);
 
+request.onupgradeneeded = ({target}) => {
+    const db = target.result;
+    const objectStore = db.createObjectstore("transactions");
+};
+
 request.onsuccess = event => {
     console.log(request.result);
 };
