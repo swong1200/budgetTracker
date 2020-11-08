9ad5955eb1114b762360d3e5e5786553b32af1ab
Stuart Wong <swong31@gmail.com>
2020-11-08 11:46:20 -0800

    deploying:

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ const config = {
     entry: "./public/index.js",
     output: {
         path: __dirname + "/public/dist/",
-        filename: "bundle.js"
+        filename: "bundle.js",
+        publicPath: "/"
     },
     mode: "production",
     plugins: [new WebpackPwaManifest({
