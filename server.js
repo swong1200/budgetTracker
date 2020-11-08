b650d36829bd601a1d353b7aeeadd51cc1130e7c
Stuart Wong <swong31@gmail.com>
2020-11-08 11:42:42 -0800

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -25,6 +25,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(port, () => {
+  console.log(`App running on port ${port}!`);
 });
